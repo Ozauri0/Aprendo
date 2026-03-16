8d37f440be307d76b48328d26788cec6847ed082
Christian <christianagr.62@gmail.com>
2026-03-16 16:40:53 -0300

    fix: linux now working

--- dist/renderer/descargas.js
+++ dist/renderer/descargas.js
@@ -125,7 +125,7 @@ async function launchAndLogin(username, password) {
     const browser = await puppeteer.launch({
         headless: true,
         defaultViewport: null,
-        args: ['--start-maximized']
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized']
     });
     const page = await browser.newPage();
     try {
