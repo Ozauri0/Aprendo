7687a9fe0c59f59d52300a823c5c6a3761c07997
Ozauri0 <christianferrer.dev@gmail.com>
2026-08-17 10:10:04 -0400

    1.4.3

--- src/renderer/components/footer.ts
+++ src/renderer/components/footer.ts
@@ -12,7 +12,7 @@ export function renderFooter(): string {
                 <span>Universidad Católica de Temuco</span>
             </div>
             <div class="footer-divider"></div>
-            <p>Aprendo UCT v1.3.1 &mdash; Desarrollado por <a class="footer-link" href="#" onclick="event.preventDefault(); require('electron').shell.openExternal('https://christianferrer.me')" aria-label="Sitio web de Christian Ferrer, abre en navegador externo">Christian Ferrer</a></p>
+            <p>Aprendo UCT v1.4.3 &mdash; Desarrollado por <a class="footer-link" href="#" onclick="event.preventDefault(); require('electron').shell.openExternal('https://christianferrer.me')" aria-label="Sitio web de Christian Ferrer, abre en navegador externo">Christian Ferrer</a></p>
         </footer>
     `;
 }
